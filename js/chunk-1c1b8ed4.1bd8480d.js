27a89b3e6f2946272ef9e3c61f5baef738a90a1f
Charles Yang <cyang@agexchangegroup.com>
2021-07-15 23:57:28 -0600

    deploy

---
+++ js/chunk-1c1b8ed4.1bd8480d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-1c1b8ed4"],{"8d9f":function(t,e,n){"use strict";n("9eb3")},"9eb3":function(t,e,n){},fd3f:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"blog"},[n("router-view"),n("router-link",{staticClass:"back",attrs:{to:"/",tag:"a"}},[t._v("« Back")])],1)},c=[],s=(n("8d9f"),n("2877")),i={},r=Object(s["a"])(i,a,c,!1,null,"00df2a6b",null);e["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-1c1b8ed4.1bd8480d.js.map
